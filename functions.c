be085bb093e37d5dddfd54e4d4b21ebf9abe37bd
Mubeen Khalid <mubeen070@gmail.com>
2021-11-02 00:33:20 +0500

    functions
    c prog

---
+++ functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+double square(double num)
+{
+    return (num * num);
+}
+
+int main()
+{
+    printf(" \n \n Funtion : Find square of any number: \n");
+    printf("------------------------------------------- \n");
+
+    int num;
+    double n;
+    printf("Enter the number to square: ");
+    scanf("%d", &num);
+    n = square(num);
+    printf("The square of %d is : %.1f", num, n);
+    return 0;
+}
